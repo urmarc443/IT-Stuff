33fbceb4924adb783fd5a907d86fecbaf070bf70
urmarc443 <urmarc443@gmail.com>
2024-04-19 09:08:48 -0700

    CHANGE MAJOR change creating hash table of adapter:MAC and removed while loop

--- NetAdapterDev.ps1
+++ NetAdapterDev.ps1
@@ -1,47 +1,32 @@
-﻿$AdapterNames = (Get-NetAdapter).Name | sort
-
+﻿$NetAdapters = (Get-NetAdapter).Name | sort
+$NetHash = $NetAdapters | ForEach-Object {@{$_ = (Get-NetAdapter -Name $_).LinkLayerAddress}}
 $DC = "172.16.1.10"
 
-$GoodNet = Test-Connection $DC -Count 1 -Quiet
-$conn = $false
-$TIMEOUT_COUNT = 0
 #Loop through IFs, Ping 172.16.1.10 (DC01) and if reply then break
-#Job can be created to achieve promise so that the other adapters can be set once the deployment moves from connecting to the domain
-#$job = Start-Job -ScriptBlock { Test-Connection -TargetName (Get-Content -Path "Servers.txt") }
-#$Results = Receive-Job $job -Wait
 
-while ($conn -eq $false)
+foreach ($adapter in $NetAdapters)
 {
-    if ($TIMEOUT_COUNT -eq 3)
+            
+    Disable-NetAdapter -Name $adapter -Confirm:$false
+
+            
+}
+foreach ($adapter in $NetAdapters)
+{
+    Enable-NetAdapter $adapter -Confirm:$false
+    sleep 3
+    $Test_DC_PING = Test-NetConnection 172.16.1.10
+    if ($Test_DC_PING.PingSucceeded -ne $null)
     {
-        $conn = $true
-        Write-Host "TIMEOUT: 3 FAILED ATTEMPTS"
+        Write-Host "Good Connection"
+
         break
     }
-    foreach ($adapter in $AdapterNames)
+    else
     {
-            
-        Disable-NetAdapter -Name $adapter -Confirm:$false
-
-            
-    }
-    foreach ($adapter in $IFNames)
-    {
-        Enable-NetAdapter $adapter -Confirm:$false
+        Write-Host "`nCouldn't connect to the DC...`n`nTrying the next adapter"
+        Disable-NetAdapter $adapter -Confirm:$false
         sleep 3
-        $Test_DC_PING = Test-NetConnection -WarningAction SilentlyContinue
-        if ($Test_DC_PING.PingSucceeded -eq $true)
-        {
-            Write-Host "Good Connection"
-            $conn = $true
-            break
-        }
-        else
-        {
-            Write-Host "`nCouldn't connect to the DC...`n`nTrying the next adapter"
-            Disable-NetAdapter $adapter -Confirm:$false
-            sleep 3
-            $TIMEOUT_COUNT += 1
-        }
+
     }
 }
