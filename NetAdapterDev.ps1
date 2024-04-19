93d4e084e122d761c8b2d0c79902842dac6664f8
urmarc443 <urmarc443@gmail.com>
2024-04-19 09:32:13 -0700

    CHANGE wht spc removal and increase sleep to 5

--- NetAdapterDev.ps1
+++ NetAdapterDev.ps1
@@ -3,30 +3,23 @@ $NetHash = $NetAdapters | ForEach-Object {@{$_ = (Get-NetAdapter -Name $_).LinkL
 $DC = "172.16.1.10"
 
 #Loop through IFs, Ping 172.16.1.10 (DC01) and if reply then break
-
 foreach ($adapter in $NetAdapters)
-{
-            
-    Disable-NetAdapter -Name $adapter -Confirm:$false
-
-            
+{        
+    Disable-NetAdapter -Name $adapter -Confirm:$false      
 }
 foreach ($adapter in $NetAdapters)
 {
     Enable-NetAdapter $adapter -Confirm:$false
-    sleep 3
+    sleep 5
     $Test_DC_PING = Test-NetConnection 172.16.1.10
-    if ($Test_DC_PING.PingSucceeded -ne $null)
+    if ($Test_DC_PING.PingSucceeded -ne $false)
     {
         Write-Host "Good Connection"
-
         break
     }
     else
     {
         Write-Host "`nCouldn't connect to the DC...`n`nTrying the next adapter"
         Disable-NetAdapter $adapter -Confirm:$false
-        sleep 3
-
     }
 }
